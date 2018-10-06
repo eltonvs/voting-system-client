b530be97d7466048a0b59dd396b144e0a0e3dd16
Elton Viana <eltonvs@outlook.com>
2018-10-06 18:25:02 -0300

    Use vue components on Home and Vote page

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <img class="logo" alt="Logo - Tribunal Superior Eleitoral" src="@/assets/logo.png">
+    <hr>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 250px;
+}
+
+hr {
+  border: 0;
+  height: 0;
+  border-top: 1px solid #00000020;
+  border-bottom: 1px solid #ffffff40;
+  margin: 60px 0;
+}
+</style>
--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="vote">
-    <h1>This is a vote page</h1>
+    <Header/>
+    <div class="content">
+      <h1>This is a vote page</h1>
+    </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'vote',
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+.vote {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  flex: 1;
+}
+</style>
