b3341512cf07529803654de86569937488645af3
Elton Viana <eltonvs@outlook.com>
2018-10-10 21:09:14 -0300

    Load results graph from API

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -2,16 +2,27 @@
   <div class="vote">
     <Header/>
     <div class="content">
-      <h1 @click="increaseOne()">Resultados da eleição</h1>
+      <h1>Resultados da eleição</h1>
+      <button class="button" @click="refreshData()"><i class="fas fa-sync"></i> Atualizar</button>
       <ResultsChart :chartData="results" :chartLabels="candidates" :width="400" :height="200"/>
     </div>
     <Footer/>
+
+    <modal
+      v-show="isModalVisible"
+      @close="closeModal"
+      :modalTitle="modalTitle"
+      :modalBody="modalBody"
+      modalClose="Fechar"
+    />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
 import ResultsChart from '@/components/ResultsChart.vue';
+import LinkButton from '@/components/LinkButton.vue';
+import Modal from '@/components/Modal.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
@@ -19,33 +30,62 @@ export default {
   components: {
     Header,
     ResultsChart,
+    LinkButton,
+    Modal,
     Footer,
+  },
+  created() {
+    this.refreshData();
   },
   data() {
     return {
-      chartData: {
-        labels: ['Bolsonaro', 'Haddad', 'Ciro', 'Amoedo', 'Boulos'],
-        datasets: [
-          {
-            backgroundColor: '#596f99',
-            data: [40, 20, 10, 50, 10],
-          },
-        ],
-      },
-      results: [40, 20, 10, 50, 10],
-      candidates: ['Bolsonaro', 'Haddad', 'Ciro', 'Amoedo', 'Boulos'],
+      isModalVisible: false,
+      modalTitle: 'Um erro aconteceu!',
+      modalBody: 'Tente novamente mais tarde',
+      baseUrl: 'http://localhost:8080/voting-system',
+      results: [],
+      candidates: [],
     };
   },
   methods: {
-    increaseOne() {
-      this.results = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * 40));
+    showModal(title, message) {
+      this.modalTitle = title;
+      this.modalBody = message;
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    refreshData() {
+      this.$http.get(`${this.baseUrl}/votes`).then(
+        (response) => {
+          if (response.status === 200) {
+            this.candidates = [];
+            this.results = [];
+            Object.entries(response.body).forEach(([candidate, votes]) => {
+              this.candidates.push(candidate);
+              this.results.push(votes);
+            });
+          }
+        },
+        () => {
+          this.showModal(
+            'Erro ao se conectar com o servidor',
+            'Não foi possível recuperar os resultados da eleição, tente novamente mais tarde.',
+          );
+        },
+      );
     },
   },
 };
 </script>
 
 <style lang="sass" scoped>
+:$black #000
+:$white #fff
+:$green #206a2b
+:$orange #e7a908
+
 .vote
   :flex 1
   :display flex
@@ -56,4 +96,18 @@ export default {
 
 h1
   :margin-bottom 60px
+
+.button
+  :display inline-block
+  :background-color $white
+  :border 1px solid rgba($black, 0.125)
+  :border-radius 0.25rem
+  :padding 10px 15px
+  :color $green
+  :font-size 16px
+  :text-decoration none
+  :transition all 0.2s ease-in-out
+
+  &:hover
+    :color $orange
 </style>
